38e3652217186112048f6d740cfae94eb19e6109
summerwise <zelyoniyoleg@yandex.ru>
2023-05-30 00:20:15 +0500

    save

--- app/src/main/java/com/coldwised/swipepix/presentation/catalog/categories/component/CategoriesTopBar.kt
+++ app/src/main/java/com/coldwised/swipepix/presentation/catalog/categories/component/CategoriesTopBar.kt
@@ -1,19 +1,24 @@
 package com.coldwised.swipepix.presentation.catalog.categories.component
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.coldwised.swipepix.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,65 +32,88 @@ fun CategoriesTopBar(
 	onBackClick: () -> Unit,
 	scrollBehavior: TopAppBarScrollBehavior,
 ) {
-	TopAppBar(
-		title = {
-			Column(
-				modifier = Modifier
-					.fillMaxWidth()
-			) {
-				TextField(
+	Column(
+		modifier = Modifier
+			.statusBarsPadding()
+			.fillMaxWidth(),
+	) {
+		CenterAlignedTopAppBar(
+			title = {
+				BasicTextField(
+					modifier = Modifier
+						.fillMaxWidth()
+						.height(40.dp),
 					value = searchQuery,
 					onValueChange = onSearchQueryChanged,
+					textStyle = TextStyle(
+						fontSize = 20.sp,
+						color = MaterialTheme.colorScheme.onSurfaceVariant,
+					),
+					cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurfaceVariant),
+					decorationBox = { innerTextField ->
+						OutlinedTextFieldDefaults.DecorationBox(
+							value = searchQuery,
+							innerTextField = innerTextField,
+							enabled = true,
+							singleLine = true,
+							visualTransformation = VisualTransformation.None,
+							interactionSource = MutableInteractionSource(),
+							placeholder = {
+								Text(
+									text = "Поиск"
+								)
+							},
+							leadingIcon = {
+								Icon(
+									imageVector = Icons.Default.Search,
+									contentDescription = null,
+									tint = MaterialTheme.colorScheme.onSurfaceVariant,
+								)
+							},
+							trailingIcon = {
+								if(searchQuery.isNotEmpty()) {
+									IconButton(
+										onClick = {/*TODO*/},
+									) {
+										Icon(
+											contentDescription = null,
+											tint = MaterialTheme.colorScheme.onSurfaceVariant,
+											imageVector = Icons.Default.Clear,
+										)
+									}
+								}
+							},
+							colors = OutlinedTextFieldDefaults.colors(),
+							contentPadding = PaddingValues(0.dp),
+							container = {
+								Box(
+									modifier = Modifier
+										.fillMaxWidth()
+										.clip(RoundedCornerShape(12.dp))
+										.background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp)),
+								)
+							},
+						)
+					},
+					singleLine = true,
 				)
-				Text(
-					title ?: stringResource(R.string.categories_topbar_title)
-				)
-			}
-		},
-		navigationIcon = {
-			if(backIconVisible) {
-				IconButton(onClick = onBackClick) {
-					Icon(
-						imageVector = Icons.Default.ArrowBack,
-						contentDescription = null
-					)
-				}
-			}
-		},
-		scrollBehavior = scrollBehavior,
-		actions = {
-			Column {
-				var menuVisible by remember {
-					mutableStateOf(false)
-				}
-				IconButton(
-					onClick = {
-						menuVisible = !menuVisible
+			},
+			navigationIcon = {
+				if(backIconVisible) {
+					IconButton(onClick = onBackClick) {
+						Icon(
+							imageVector = Icons.Default.ArrowBack,
+							contentDescription = null
+						)
 					}
-				) {
-					Icon(
-						imageVector = Icons.Default.MoreVert,
-						contentDescription = null,
-					)
 				}
-				DropdownMenu(
-					expanded = menuVisible,
-					onDismissRequest = {
-						menuVisible = false
-					}
-				) {
-					DropdownMenuItem(
-						modifier = Modifier.fillMaxSize()
-						,
-						text = {
-							Text(
-								text = stringResource(R.string.theme_label_text)
-							)
-						},
-						onClick = onThemeSettingsClick
-					)
-				}
-			}
-		}
-	)
+			},
+			scrollBehavior = scrollBehavior,
+		)
+		Text(
+			modifier = Modifier.padding(start = 12.dp),
+			text = title ?: stringResource(R.string.categories_topbar_title),
+			style = MaterialTheme.typography.titleLarge,
+		)
+	}
 }
